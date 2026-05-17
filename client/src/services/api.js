0cbb5b60def523a21caa3c2642a023b26b02f5e9
prince200926-lab <prince200926@gmail.com>
2026-05-17 17:44:43 +0530

    first

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://tracker-h319.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
